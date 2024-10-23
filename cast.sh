4fbdb3dc3d54abea4ae196fd7a845c20c5b39634
Peter Sjolin <pigfoxeria@gmail.com>
2024-10-22 19:22:08 -0700

    wip

--- cast.sh
+++ cast.sh
@@ -9,7 +9,7 @@ echo "Calling contract"
 #cast call "$Dex1" "getName()(string)" --rpc-url "$SEPOLIA_RPC_URL"
 #$ cast send --private-key <Your Private Key> 0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc $(cast from-utf8 "hello world") --rpc-url http://127.0.0.1:8545/
 cast send "$Dex1" "setTokenPrice(address,uint256)" "$ERC20Token" "66669" --rpc-url "$SEPOLIA_RPC_URL" --private-key "$PRIVATE_KEY"
-echo "-------------------------"
+echo "-----------getTokenPrice--------------"
 hex_value=$(cast call "$Dex1" "getTokenPrice(address)" "$ERC20Token" --rpc-url "$SEPOLIA_RPC_URL")
 
 # Clean the output by stripping '0x' and any extra whitespace
@@ -23,3 +23,4 @@ else
     decimal_value=$(printf "%d\n" "0x$hex_value")
     echo "Decimal value: $decimal_value"
 fi
+echo "-----------end getTokenPrice--------------"
