3e9be3b9d9afca3e294715f8c8c0ac36f0a4db0e
Peter Sjolin <pigfoxeria@gmail.com>
2024-10-23 18:22:59 -0700

    wip

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ set +x
 set +e
 clear
 . ./.env
-
+forge clean
+echo "Deploying..."
 contract="ERC20Token"
-forge script script/"$ERC20Token".s.sol:"$ERC20Token"Script --rpc-url "$SEPOLIA_RPC_URL" --private-key "$PRIVATE_KEY" --broadcast --verify
+forge script script/"$contract".s.sol:"$contract"Script --rpc-url "$SEPOLIA_RPC_URL" --private-key "$PRIVATE_KEY" --broadcast --verify --optimize 200
