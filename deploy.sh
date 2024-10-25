588cd9a4ff37af241f0e07a783758bbe061f2ac9
Peter Sjolin <pigfoxeria@gmail.com>
2024-10-25 14:18:07 -0700

    wip

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,7 @@ clear
 . ./.env
 forge clean
 echo "Deploying..."
-contract="Dex"
-forge script script/"$contract".s.sol:"$contract"Script --rpc-url "$SEPOLIA_RPC_URL" --private-key "$PRIVATE_KEY" --broadcast --verify --optimize 200
+rpc_url="$SEPOLIA_RPC_URL"
+private_key="$PRIVATE_KEY"
+contract="Vault"
+forge script script/"$contract".s.sol:"$contract"Script --rpc-url "$rpc_url" --private-key "$private_key" --broadcast --verify --optimize 200
