fc5e1da907b89d1186f3dfc31ba98cb4ecdb4d9b
Peter Sjolin <pigfoxeria@gmail.com>
2024-10-28 20:12:19 -0700

    wip

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ contract="Arbitrage"
 function="swapTokens"
 rpc_url=$SEPOLIA_HTTP_RPC_URL
 echo "Testing $contract::$function..."
-#forge test --rpc-url "$rpc_url" --gas-report --verbosity --match-contract "$contract" --match-test "$function" -vvvv
+forge test --rpc-url "$rpc_url" --gas-report --verbosity --match-contract "$contract" --match-test "$function" -vvvv
 #forge test --fork-url "$rpc_url" --etherscan-api-key "$ETHERSCAN_API_KEY" --gas-report --verbosity --match-contract "$contract" --match-test "$function" -vvvv
-forge test --gas-report --verbosity --match-contract "$contract" --match-test "$function" -vvvv
+#forge test --gas-report --verbosity --match-contract "$contract" --match-test "$function" -vvvv
 
 
