1ba7819ed90f2cb588d7c8fb609b3c5f75db1e3f
Peter Sjolin <pigfoxeria@gmail.com>
2024-11-30 11:06:04 -0800

    wip

--- slither.sh
+++ slither.sh
@@ -9,9 +9,12 @@ clear
 # Clean up any previous Forge build artifacts
 forge clean
 
+contract="Pigfox"
+forge flatten src/"$contract".sol --output flattened/"$contract"Flattened.sol
+
 # Analyze the contract with Slither
 docker pull trailofbits/eth-security-toolbox
 
 # Ensure the correct path is passed to Slither
 docker run --rm -v "$(pwd):/project" trailofbits/eth-security-toolbox \
-    slither /project/src/Proxy.sol
+    slither /project/flattened/"$contract"Flattened.sol
