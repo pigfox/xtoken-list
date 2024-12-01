189aa7d6bcbfada94a61278cdea7bd92cf0d4208
Peter Sjolin <pigfoxeria@gmail.com>
2024-11-30 16:54:19 -0800

    wip

--- slither.sh
+++ slither.sh
@@ -11,10 +11,14 @@ forge clean
 
 contract="Pigfox"
 forge flatten src/"$contract".sol --output flattened/"$contract"Flattened.sol
-
+echo "Slithering $contract..."
 # Analyze the contract with Slither
 docker pull trailofbits/eth-security-toolbox
 
 # Ensure the correct path is passed to Slither
-docker run --rm -v "$(pwd):/project" trailofbits/eth-security-toolbox \
-    slither /project/flattened/"$contract"Flattened.sol
+#docker run --rm -v "$(pwd):/project" trailofbits/eth-security-toolbox slither /project/flattened/"$contract"Flattened.sol
+
+echo "Myth $contract..."
+docker pull mythril/myth
+docker run -v $(pwd):/project mythril/myth analyze /project/flattened/"$contract"Flattened.sol
+
