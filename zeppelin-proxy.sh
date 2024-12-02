bc4e80bbc4fe3240b054e914f4378e204ebf6b81
Peter Sjolin <pigfoxeria@gmail.com>
2024-12-01 17:00:31 -0800

    wip

--- zeppelin-proxy.sh
+++ zeppelin-proxy.sh
@@ -4,3 +4,16 @@ set -e
 clear
 . ./.env
 forge clean
+rpc_url="$SEPOLIA_HTTP_RPC_URL"
+private_key="$PRIVATE_KEY"
+
+cast send "$ZEPPELIN_PROXY" "setValue(uint256)" 42 --rpc-url "$rpc_url" --from "$WALLET_ADDRESS"  --private-key "$private_key"
+#cast call "$ZEPPELIN_PROXY" "value()(uint256)"
+#cast send $ZEPPELIN_PROXY "upgradeTo(address)" $ZEPPELIN_IMPL_V2 --from $WALLET_ADDRESS
+#cast send $ZEPPELIN_PROXY "initialize(address)" $WALLET_ADDRESS --from $WALLET_ADDRESS
+#cast send $ZEPPELIN_PROXY "setValue(uint256)" 100 --from $WALLET_ADDRESS
+#cast call $ZEPPELIN_PROXY "value()(uint256)"
+#cast call $ZEPPELIN_PROXY "owner()(address)"
+#cast call $ZEPPELIN_PROXY "value()(uint256)" --rpc-url "$rpc_url"
+
+
