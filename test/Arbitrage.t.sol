14b6b0ac0a27f0b707f2dba6d9a9999ce89d38ea
peter Sjolin <pigfoxeria@gmail.com>
2025-03-23 17:53:40 -0700

    console vs console2 separation

--- test/Arbitrage.t.sol
+++ test/Arbitrage.t.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.26;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "forge-std/console2.sol";
 import "../src/PigfoxToken.sol";
 import "../src/IDex.sol";
 import "../src/Arbitrage.sol";
@@ -52,7 +54,8 @@ contract ArbitrageTest is Test {
 
         // Check and ensure wallet has PFX tokens
         uint256 walletBalance = pigfoxToken.balanceOf(wallet);
-        console.log("Wallet PFX Balance:", walletBalance);
+        console.log("Wallet PFX Balance:");
+        console2.log( walletBalance);
         if (walletBalance < 100 * 10**18) {
             console.log("Warning: Wallet has insufficient PFX. Attempting to mint...");
             try pigfoxToken.mint(100 * 10**18) {
@@ -67,8 +70,10 @@ contract ArbitrageTest is Test {
         // Check DEX balances and fund if empty
         uint256 dex1Balance = pigfoxToken.balanceOf(dex1Addr);
         uint256 dex2Balance = pigfoxToken.balanceOf(dex2Addr);
-        console.log("DEX1 PFX Balance:", dex1Balance);
-        console.log("DEX2 PFX Balance:", dex2Balance);
+        console.log("DEX1 PFX Balance:");
+        console2.logUint(dex1Balance);
+        console.log("DEX2 PFX Balance:");
+        console2.logUint(dex2Balance);
 
         if (dex1Balance < 50 * 10**18) {
             pigfoxToken.approve(dex1Addr, 50 * 10**18);
@@ -83,7 +88,8 @@ contract ArbitrageTest is Test {
 
         // Fund arbitrage contract with ETH
         uint256 arbitrageEth = address(arbitrage).balance;
-        console.log("Arbitrage ETH Balance:", arbitrageEth);
+        console.log("Arbitrage ETH Balance:");
+        console2.log( arbitrageEth);
         if (arbitrageEth < 1 ether) {
             vm.deal(wallet, 2 ether); // Ensure wallet has ETH for funding
             (bool sent, ) = address(arbitrage).call{value: 1 ether}("");
@@ -118,14 +124,18 @@ contract ArbitrageTest is Test {
         // Log initial state
         uint256 initialEth = address(arbitrage).balance;
         uint256 initialProfitEth = wallet.balance;
-        console.log("Initial Arbitrage ETH:", initialEth);
-        console.log("Initial Wallet ETH:", initialProfitEth);
+        console.log("Initial Arbitrage ETH:");
+        console2.log( initialEth);
+        console.log("Initial Wallet ETH:");
+        console2.log(initialProfitEth);
 
         // Check prices using getTokenPriceOf
         uint256 dex1Price = dex1.getTokenPrice(address(pigfoxToken));
         uint256 dex2Price = dex2.getTokenPrice(address(pigfoxToken));
-        console.log("DEX1 Price (wei/PFX):", dex1Price);
-        console.log("DEX2 Price (wei/PFX):", dex2Price);
+        console.log("DEX1 Price (wei/PFX):");
+        console2.log(dex1Price);
+        console.log("DEX2 Price (wei/PFX):");
+        console2.log(dex2Price);
 
         // Ensure arbitrage opportunity exists
         require(dex2Price < dex1Price, "No arbitrage opportunity: DEX2 price >= DEX1 price");
