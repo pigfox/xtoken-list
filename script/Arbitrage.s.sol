b7ea52154613f0cc43286f688c79c38f9ff6f68c
Peter Sjolin <pigfoxeria@gmail.com>
2025-03-29 13:08:09 -0700

    wip

--- script/Arbitrage.s.sol
+++ script/Arbitrage.s.sol
@@ -7,7 +7,7 @@ import "../src/Arbitrage.sol";
 contract ArbitrageScript is Script {
     function run() external {
         // Read the vault address from the .env file
-        address vaultAddress = vm.envAddress("VAULT_ADDRESS");
+        address vaultAddress = vm.envAddress("VAULT");
 
         vm.startBroadcast();
         Arbitrage arbitrage = new Arbitrage(vaultAddress);
