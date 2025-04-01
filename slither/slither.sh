10641bbde7196d9a335d49cf149cc11bb274a76e
Peter Sjolin <pigfoxeria@gmail.com>
2025-03-31 22:10:27 -0700

    WIP

--- slither/slither.sh
+++ slither/slither.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+clear
 # Configuration
 VERSION="0.8.26"
 CONTRACT="Arbitrage"
+SOLC_VERSION="0.8.26"
 
 # Exit on any error
 set -e
@@ -10,14 +11,17 @@ set -e
 # Print commands as they are executed (for debugging)
 set -x
 
-# Create and activate virtual environment
+# Create and activate virtual environment in the root directory
+cd ..
 python3 -m venv venv
 source venv/bin/activate
 
-# Install and configure solc-select
-solc-select install
-solc-select install "$VERSION"
-solc-select use "$VERSION"
+# Install solc-select
+pip install solc-select
+
+# Install and configure solc version
+solc-select install "$SOLC_VERSION"
+solc-select use "$SOLC_VERSION"
 
 # Verify solc version
 solc --version
@@ -28,11 +32,9 @@ pip3 install slither-analyzer
 # Verify Slither version
 slither --version
 
-# Run Slither on the contract with OpenZeppelin remapping
-slither "../src/$CONTRACT.sol" --solc-remaps "@openzeppelin/contracts/=../lib/openzeppelin-contracts/contracts/"
+# Run Slither with absolute paths
+slither "src/$CONTRACT.sol" --solc-remaps "@openzeppelin/contracts=/home/peter/Documents/crypto-projects/arbitrage-bot-v2-snippets/lib/openzeppelin-contracts/contracts"
 
-# Optionally deactivate the virtual environment (uncomment if desired)
-deactivate
 # Clear previous installations
 rm -rf venv
 
---
+++ src/test.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.8.26;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+contract Test {
+    IERC20 public token;
+}
